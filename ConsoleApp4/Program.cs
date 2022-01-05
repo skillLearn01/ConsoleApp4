4a0cda38ddca94d05a1c9fae8036309b68b872d1
PC <PC@DESKTOP-U995AMT>
2022-01-05 18:07:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+
+		string MyName = "Jane";
+		byte MyAge = 27;
+		bool HaveIApet = true;
+		double MyShoeSize = 37.5;
+
+		Console.WriteLine("My name is " + MyName);
+		Console.WriteLine("MyAge " + MyAge);
+		Console.WriteLine("Do I have a pet? " + HaveIApet);
+		Console.WriteLine("My shoe size is " + MyShoeSize);
+
+	}
+}
